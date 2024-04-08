30c26df35bbe018cb82750d99ada9876c7d4ee06
fcchbjm <2748195351@qq.com>
2024-04-08 11:10:11 +0800

    upload

--- SeqList/test.c
+++ SeqList/test.c
@@ -3,59 +3,59 @@
 void SLtest(void)
 {
 	SL sl;
-	SLInit(&sl);
+	SLInit(&sl);//初始化
 	
 	for (int i = 0; i < 10; i++)
 	{
-		SLPushBack(&sl, i);
+		SLPushBack(&sl, i);//尾插1~9
 		SLPrint(sl);
 	}
 
 	for (int i = 0; i < 10; i++)
 	{
-		SLPushFront(&sl, i);
+		SLPushFront(&sl, i);//头插1~9
 		SLPrint(sl);
 	}
 
-	SLPopFront(&sl);
+	SLPopFront(&sl);//头删
 	SLPrint(sl);
 
-	SLPopBack(&sl);
+	SLPopBack(&sl);//尾删
 	SLPrint(sl);
 
-	SLDestory(&sl);
+	SLDestory(&sl);//销毁
 }
 
 void SLtest2()
 {
 	SL sl;
-	SLInit(&sl);
+	SLInit(&sl);//初始化
 
 	for (int i = 0; i < 10; i++)
 	{
-		SLPushBack(&sl, i);
+		SLPushBack(&sl, i);//尾插1~9
 	}
 	SLPrint(sl);
 
-	SLErase(&sl, 0);
+	SLErase(&sl, 0);//删掉第一个元素
 	SLPrint(sl);
 
-	SLInsert(&sl, 1, 10);
+	SLInsert(&sl, 1, 10);//在第二个元素前插入10
 	SLPrint(sl);
 
 	int ret = 0;
-	ret = SLFind(&sl, 5);
+	ret = SLFind(&sl, 5);//查找5这个元素
 	printf("ret=%d\n", ret);
 
-	ret = SLFind(&sl, 11);
+	ret = SLFind(&sl, 11);//查找11这个元素
 	printf("ret=%d\n", ret);
 
-	SLDestory(&sl);
+	SLDestory(&sl);//销毁
 }
 
 int main()
 {
-	//SLtest();
+	SLtest();
 	SLtest2();
 	return 0;
 }
